1bbbf09bcb02c4399e496f4176d1755704215900
Kirill Rudenko @swifty94 <kirillrudenko1994@gmail.com>
2022-05-02 19:22:22 +0300

    initial commit

---
+++ DoneItem.cs
@@ -0,0 +1,8 @@
+using System;
+public class DoneItem
+{
+    #nullable enable
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+    
+}
---
+++ TodoItem.cs
@@ -0,0 +1,8 @@
+using System;
+public class TodoItem
+{
+    #nullable enable
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+    
+}
